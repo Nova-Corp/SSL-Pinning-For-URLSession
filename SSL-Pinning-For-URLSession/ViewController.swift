dc4e2e5c65a0745d5b2805e7cda9b67ba99adfcf
Shanmugarajeshwaran R <shanmugarajeshwaran@sparkouttech.com>
2021-09-30 18:28:59 +0530

    Initial Commit

---
+++ SSL-Pinning-For-URLSession/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SSL-Pinning-For-URLSession
+//
+//  Created by Mac on 30/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
